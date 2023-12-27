b36f8c08f60c8b7e76905c4a4d1a06d1ed8190e2
bdunai123 <b.o.dunai@khai.edu>
2023-12-28 00:30:18 +0200

    CoffeeMachine 1-st stage

---
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -0,0 +1,41 @@
+public class CoffeeMachine {
+
+    public static void main(String[] args) {
+        makeEspresso();
+        makeCappuccino();
+        makeLatte();
+    }
+
+    public static void makeEspresso() {
+        System.out.println("Making espresso:");
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Coffee is ready!\n");
+    }
+
+    public static void makeCappuccino() {
+        System.out.println("Making cappuccino:");
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Adding foam to the coffee");
+        System.out.println("Cappuccino is ready!\n");
+    }
+
+    public static void makeLatte() {
+        System.out.println("Making latte:");
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Latte is ready!\n");
+    }
+}
