fa196190976cd1c4e5f7d37e818681bf964d5410
bdunai123 <b.o.dunai@khai.edu>
2023-12-28 00:14:19 +0200

    hangman 7-st stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,100 +1,104 @@
-import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class hangman {
-    public static void main(String[] args) {
-        // Список слів
+    public static void displayIntro() {
+        System.out.println("HANGMAN");
+        System.out.println("Welcome to the Hangman Game!");
+        System.out.println("The game will be available soon.");
+        System.out.println();
+    }
+
+    public static String chooseWord() {
         String[] words = {"python", "java", "javascript", "kotlin"};
+        Random random = new Random();
+        return words[random.nextInt(words.length)];
+    }
 
-        // Виведення привітання
-        displayIntro();
-
-        // Випадковий вибір слова
-        String secretWord = chooseWord(words);
-
-        // Ініціалізація виведеного слова
-        StringBuilder displayedWord = new StringBuilder();
-        for (int i = 0; i < secretWord.length(); i++) {
-            displayedWord.append("-");
+    public static String displayWord(String word, String[] guessedLetters) {
+        StringBuilder display = new StringBuilder();
+        for (char letter : word.toCharArray()) {
+            if (containsLetter(guessedLetters, letter)) {
+                display.append(letter);
+            } else {
+                display.append('-');
+            }
         }
+        return display.toString();
+    }
 
-        // Виведення вказівки для гравця
-        System.out.println(displayedWord);
+    public static boolean containsLetter(String[] array, char letter) {
+        for (String str : array) {
+            if (str != null && str.length() == 1 && str.charAt(0) == letter) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        // Введення користувачем букв
+    public static void playHangman() {
+        String wordToGuess = chooseWord();
+        String[] guessedLetters = new String[26];
+        int attemptsLeft = 8;
+
+        System.out.println(displayWord(wordToGuess, guessedLetters));
+
         Scanner scanner = new Scanner(System.in);
+        while (attemptsLeft > 0) {
+            System.out.print("Input a letter: > ");
+            String userInput = scanner.next().toLowerCase();
 
-        // Лічильник життів
-        int lives = 8;
+            if (userInput.equals("exit")) {
+                break;
+            } else if (userInput.equals("play")) {
+                playHangman();
+            }
 
-        // Масив для зберігання вже введених букв
-        boolean[] guessedLetters = new boolean[26];
-
-        while (lives > 0) {
-            // Введення букви
-            System.out.print("Input a letter: ");
-            char guessedLetter = scanner.next().charAt(0);
-
-            // Перевірка, чи буква вже вводилась
-            if (guessedLetters[guessedLetter - 'a']) {
-                System.out.println("You already guessed that letter. Try again.");
+            if (userInput.length() != 1 || !Character.isLetter(userInput.charAt(0))) {
+                System.out.println("Please enter a lowercase English letter.");
                 continue;
             }
 
-            // Додавання букви до вже введених
-            guessedLetters[guessedLetter - 'a'] = true;
+            char inputLetter = userInput.charAt(0);
 
-            // Перевірка, чи буква є в слові
-            if (checkLetter(secretWord, guessedLetter)) {
-                // Перевірка, чи гравець вгадав всі літери і має ще спроби
-                if (displayedWord.indexOf("-") == -1 && lives > 0) {
-                    System.out.println("You guessed the word!");
+            if (containsLetter(guessedLetters, inputLetter)) {
+                System.out.println("You've already guessed this letter");
+            } else if (wordToGuess.contains(userInput)) {
+                guessedLetters[inputLetter - 'a'] = userInput;
+                System.out.println(displayWord(wordToGuess, guessedLetters));
+                if (!displayWord(wordToGuess, guessedLetters).contains("-")) {
+                    System.out.println("You guessed the word " + wordToGuess + "!");
                     System.out.println("You survived!");
                     break;
                 }
-
-                // Оновлення виведеного слова після введення правильної букви
-                updateDisplayedWord(secretWord, displayedWord, guessedLetter);
-                System.out.println(displayedWord);
             } else {
-                // Зменшення кількості спроб і виведення відповідного повідомлення
-                lives--;
-                if (lives > 0) {
-                    System.out.println("That letter doesn't appear in the word");
-                    System.out.println(displayedWord);
-                } else {
-                    System.out.println("You lost!");
-                }
+                attemptsLeft--;
+                System.out.println("That letter doesn't appear in the word");
+                System.out.println("Attempts left: " + attemptsLeft);
+                System.out.println(displayWord(wordToGuess, guessedLetters));
+            }
+
+            if (attemptsLeft == 0) {
+                System.out.println("You lost!");
             }
         }
-
-        // Закриття сканера
-        scanner.close();
     }
 
-    // Функція для виведення анонсу гри
-    private static void displayIntro() {
-        System.out.println("HANGMAN");
-        System.out.println("Welcome to the Hangman Game!");
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    // Функція для вибору випадкового слова зі списку
-    private static String chooseWord(String[] words) {
-        Random random = new Random();
-        int index = random.nextInt(words.length);
-        return words[index];
-    }
+        while (true) {
+            displayIntro();
+            System.out.print("Type \"play\" to play the game, \"exit\" to quit: > ");
+            String userChoice = scanner.next().toLowerCase();
 
-    // Функція для перевірки, чи буква є в слові
-    private static boolean checkLetter(String word, char letter) {
-        return word.indexOf(letter) != -1;
-    }
-
-    // Функція для оновлення виведеного слова після введення правильної букви
-    private static void updateDisplayedWord(String secretWord, StringBuilder displayedWord, char guessedLetter) {
-        for (int i = 0; i < secretWord.length(); i++) {
-            if (secretWord.charAt(i) == guessedLetter) {
-                displayedWord.setCharAt(i, guessedLetter);
+            if (userChoice.equals("exit")) {
+                break;
+            } else if (userChoice.equals("play")) {
+                playHangman();
+            } else {
+                System.out.println("Invalid choice. Please enter \"play\" or \"exit\".");
             }
         }
     }
