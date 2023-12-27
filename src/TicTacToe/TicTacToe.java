7ae1d465b9ba2330b9159e561ff8cd23c550e391
bdunai123 <b.o.dunai@khai.edu>
2023-12-28 00:54:05 +0200

    TicTacToe 2-nd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,23 +1,43 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        // Ігрове поле
-        char[][] gameBoard = {
-                {'X', 'O', 'X'},
-                {'O', 'X', 'O'},
-                {'X', 'X', 'O'}
-        };
+        Scanner scanner = new Scanner(System.in);
 
-        // Виведення ігрового поля
+        System.out.print("Enter cells: ");
+        String userInput = scanner.nextLine();
+
+        char[][] gameBoard = createGameBoard(userInput);
         printGameBoard(gameBoard);
+    }
+
+    // Метод для створення ігрового поля на основі введеного рядка
+    public static char[][] createGameBoard(String input) {
+        char[][] board = new char[3][3];
+        int index = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = input.charAt(index);
+                index++;
+            }
+        }
+
+        return board;
     }
 
     // Метод для виведення ігрового поля
     public static void printGameBoard(char[][] board) {
+        System.out.println("---------");
+
         for (int i = 0; i < board.length; i++) {
+            System.out.print("| ");
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("|");
         }
+
+        System.out.println("---------");
     }
 }
