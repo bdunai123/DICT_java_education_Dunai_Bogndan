139a8a211db10aa1355ac83384d6e5f049e3631a
bdunai123 <b.o.dunai@khai.edu>
2023-12-27 23:18:37 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,25 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        // Етап 1-st stage: Привітання бота
+        String botName = "JavaBot";
+        int birthYear = java.time.Year.now().getValue();
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+
+        // Етап 2-nd stage: Представлення боту
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+
+        // Зчитування ім'я користувача зі стандартного введення
+        String userName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + userName + "!");
+    }
 }
+
+
