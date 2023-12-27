dd321a815ddb44744b291fafa1ac56372d3f0c82
bdunai123 <b.o.dunai@khai.edu>
2023-12-27 23:53:30 +0200

    hangman 2-nd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,2 +1,33 @@
-package Hangman;public class hangman {
+import java.util.Scanner;
+
+public class hangman {
+    public static void main(String[] args) {
+        // Виведення привітання
+        displayIntro();
+
+        // Загадане слово
+        String secretWord = "java";
+
+        // Виведення вказівки для гравця
+        System.out.print("Guess the word: ");
+
+        // Введення користувачем слова
+        Scanner scanner = new Scanner(System.in);
+        String guessedWord = scanner.next();
+
+        // Перевірка правильності введеного слова
+        if (guessedWord.equals(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
+    }
+
+    // Функція для виведення анонсу гри
+    private static void displayIntro() {
+        System.out.println("HANGMAN");
+        System.out.println("Welcome to the Hangman Game!");
+    }
 }
